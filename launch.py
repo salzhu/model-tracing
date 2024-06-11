550a7f56d23b959bd8a199beacb0e83e873e4374
RohithKuditipudi <rohith.kuditipudi@gmail.com>
2024-06-11 16:10:34 -0700

    create directories

--- launch.py
+++ launch.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser(description="Experiment Settings")
 parser.add_argument('--slurm',default="nlprun -g 1 -d a6000 -r 80G -a model-tracing",type=str)
 parser.add_argument('--python',default="python experiment.py",type=str)
 parser.add_argument('--models',default="model_list.yaml",type=str)
-parser.add_argument('--save',default="./",type=str)
+parser.add_argument('--save',default=".",type=str)
 
 args = parser.parse_args()
 
@@ -17,12 +17,15 @@ model_paths = yaml.load(open(args.models, 'r'), Loader=Loader)
 base_models = model_paths["base_models"]
 ft_models = model_paths["ft_models"]
 
+subprocess.run(f"mkdir -p {args.save}/logs")
+subprocess.run(f"mkdir -p {args.save}/results")
+
 for base_model in base_models:
     for ft_model in ft_models:
         job_id = base_model.replace("/","-") + "_AND_" + ft_model.replace("/","-")
 
-        log_path = args.save + "logs/" + job_id + ".out"
-        results_path = args.save + "results/" + job_id + ".p"
+        log_path = args.save + "/logs/" + job_id + ".out"
+        results_path = args.save + "/results/" + job_id + ".p"
 
         job = args.slurm + f" -o {log_path} -n {job_id}" \
             f" '{args.python}" + f" --base_model_id {base_model} --ft_model_id {ft_model} --save {results_path}'"
