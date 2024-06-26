257470b29c1889e9239d271ea60874fd75fd34ae
RohithKuditipudi <rohith.kuditipudi@gmail.com>
2024-06-26 00:03:07 -0700

    fix emb

--- tracing/statistics/emb.py
+++ tracing/statistics/emb.py
@@ -5,7 +5,7 @@ import torch
 from ..utils.utils import cossim
 
 def statistic(base_model,ft_model):
-    base_mat = base_model.state_dict()['model.layers.'+str(i)+'.mlp.gate_proj.weight'][:VOCAB_SIZE]
-    ft_mat = ft_model.state_dict()['model.layers.'+str(i)+'.mlp.gate_proj.weight'][:VOCAB_SIZE]
+    base_mat = base_model.state_dict()['model.embed_tokens.weight'][:VOCAB_SIZE]
+    ft_mat = ft_model.state_dict()['model.embed_tokens.weight'][:VOCAB_SIZE]
     
     return torch.median(torch.max(cossim(base_mat,ft_mat),axis=-1).values)
