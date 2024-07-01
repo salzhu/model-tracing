f8a72032712257f08af777003080bf992c813220
Sally Zhu <salzhu@sc.stanford.edu>
2024-07-01 14:37:22 -0700

    add --flat flag for all model pairs

--- launch.py
+++ launch.py
@@ -8,25 +8,44 @@ parser = argparse.ArgumentParser(description="Experiment Settings")
 
 parser.add_argument('--slurm',default="nlprun -g 1 -d a6000 -r 80G -a model-tracing",type=str)
 parser.add_argument('--python',default="python experiment.py",type=str)
-parser.add_argument('--models',default="model_list.yaml",type=str)
+parser.add_argument('--models',default="config/llama_flat.yaml",type=str)
 parser.add_argument('--save',default=".",type=str)
+parser.add_argument('--flat',default=True,type=bool)
 
 args = parser.parse_args()
 
 model_paths = yaml.load(open(args.models, 'r'), Loader=Loader)
-base_models = model_paths["base_models"]
-ft_models = model_paths["ft_models"]
 
 subprocess.run(f"mkdir -p {args.save}/logs",shell=True)
 subprocess.run(f"mkdir -p {args.save}/results",shell=True)
 
-for base_model in base_models:
-    for ft_model in ft_models:
-        job_id = base_model.replace("/","-") + "_AND_" + ft_model.replace("/","-")
+if args.flat:
+    for i in range(len(model_paths)):
+        for j in range(i+1,len(model_paths)):
+            model_a = model_paths[i]
+            model_b = model_paths[j]
 
-        log_path = args.save + "/logs/" + job_id + ".out"
-        results_path = args.save + "/results/" + job_id + ".p"
+            job_id = model_a.replace("/","-") + "_AND_" + model_b.replace("/","-")
 
-        job = args.slurm + f" -o {log_path} -n {job_id}" \
-            f" '{args.python}" + f" --base_model_id {base_model} --ft_model_id {ft_model} --save {results_path}'"
-        subprocess.run(job,shell=True)
+            log_path = args.save + "/logs/" + job_id + ".out"
+            results_path = args.save + "/results/" + job_id + ".p"
+
+            job = args.slurm + f" -o {log_path} -n {job_id}" \
+                f" '{args.python}" + f" --base_model_id {model_a} --ft_model_id {model_b} --save {results_path}'"
+            subprocess.run(job,shell=True)
+
+else: 
+    base_models = model_paths["base_models"]
+    ft_models = model_paths["ft_models"]
+
+    for base_model in base_models:
+        for ft_model in ft_models:
+            job_id = base_model.replace("/","-") + "_AND_" + ft_model.replace("/","-")
+
+            log_path = args.save + "/logs/" + job_id + ".out"
+            results_path = args.save + "/results/" + job_id + ".p"
+
+            job = args.slurm + f" -o {log_path} -n {job_id}" \
+                f" '{args.python}" + f" --base_model_id {base_model} --ft_model_id {ft_model} --save {results_path}'"
+            subprocess.run(job,shell=True)
+
