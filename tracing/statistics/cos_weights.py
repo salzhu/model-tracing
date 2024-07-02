2270b7eb6ef36941e50633eff18aff53c4af4b4f
Sally Zhu <salzhu@sc.stanford.edu>
2024-07-02 16:08:02 -0700

    skip attention matrices

--- tracing/statistics/cos_weights.py
+++ tracing/statistics/cos_weights.py
@@ -13,7 +13,6 @@ def cos_cor_layer_2d(base_model,ft_model,layer_name):
 
     base_mat = base_model.state_dict()[layer_name]
     ft_mat = ft_model.state_dict()[layer_name]
-
     matched = torch.argmax(cossim(base_mat,ft_mat),axis=-1)
 
     orig = torch.arange(len(matched))
@@ -34,28 +33,34 @@ def cos_cor_fisher(model1,model2):
         if name1 != name2:
             raise ValueError(f"Model parameter names do not match: {name1} != {name2}")
         elif param1.dim() == 1: continue
+        elif "attn" in name1: continue
         elif param1.shape != param2.shape:
             if name1 == "model.embed_tokens.weight" or name1 == "lm_head.weight":
                 print(
                     f"Skipping {name1} because of shape mismatch: {param1.shape} != {param2.shape}"
                 )
+                p_values.append(np.nan)
                 continue
             raise ValueError(
                 f"Model parameter shapes do not match for {name1}: {param1.shape} != {param2.shape}"
             )
-
         pvalue = cos_cor_layer_2d(model1, model2, name1)
-        chi_squared -= 2 * np.log(pvalue)
-        num_layers += 1
+        if not np.isnan(pvalue):
+            chi_squared -= 2 * np.log(pvalue)
+            num_layers += 1
         p_values.append(pvalue)
 
+        # print(name1, pvalue)
+
     p_value = chi2.sf(chi_squared, df=2*num_layers)
+    print(p_value)
+    print(p_values)
     return p_value, p_values
         
 if __name__ == "__main__":
 
-    base_model_name = "meta-llama/Llama-2-7b-hf"
-    ft_model_name = "lmsys/vicuna-7b-v1.1" # "codellama/CodeLlama-7b-hf"
+    base_model_name = 'openlm-research/open_llama_7b' # 'lmsys/vicuna-7b-v1.5' # "meta-llama/Llama-2-7b-hf"
+    ft_model_name = 'openlm-research/open_llama_7b_v2' # 'LLM360/Amber' # "lmsys/vicuna-7b-v1.1" # "codellama/CodeLlama-7b-hf"
 
     base_model = AutoModelForCausalLM.from_pretrained(base_model_name, torch_dtype=torch.bfloat16)
     ft_model = AutoModelForCausalLM.from_pretrained(ft_model_name, torch_dtype=torch.bfloat16)
