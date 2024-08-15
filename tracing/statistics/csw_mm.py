e8e473cad1d8ccf9535b2c309aff03f72c7bf8f2
Sally Zhu <salzhu@sc.stanford.edu>
2024-08-15 11:38:52 -0700

    clean up

--- tracing/statistics/csw_mm.py
+++ tracing/statistics/csw_mm.py
@@ -1,5 +1,4 @@
 import torch
-from transformers import AutoModelForCausalLM
 
 from tracing.utils.utils import cossim
 
@@ -14,46 +13,3 @@ def statistic(base_model,ft_model,n_blocks=32):
         all.append(torch.median(torch.max(cossim(base_mat,ft_mat),axis=-1).values).item())
     
     return sum / len(all), all
-
-def cos_matrices(matrix_1, matrix_2):
-    # print(matrix_1.shape)
-    # print(matrix_2.shape)
-    # print(torch.max(cossim(matrix_1,matrix_2),axis=-1).values)
-    # print(cossim(matrix_1,matrix_2))
-    # print(matrix_1)
-    # print(matrix_2)
-
-    for i in range(len(matrix_1)):
-        if torch.linalg.norm(matrix_1[i]) == 0: 
-            print(i)
-
-    print("---")
-    
-    for i in range(len(matrix_2)):
-        if torch.linalg.norm(matrix_2[i]) == 0: 
-            print(i)
-
-    return torch.median(torch.max(cossim(matrix_1,matrix_2),axis=-1).values).item()
-
-if __name__ == "__main__":
-
-    # mat1 = torch.rand(11008, 4096)
-    # mat2 = torch.rand(11008, 4096)
-
-    # print(cos_matrices(mat1, mat2))
-
-    base_model_name = "meta-llama/Llama-2-7b-hf" # 'openlm-research/open_llama_7b' # 'lmsys/vicuna-7b-v1.5' 
-    ft_model_name = "lmsys/vicuna-7b-v1.1" # "codellama/CodeLlama-7b-hf" # 'openlm-research/open_llama_7b_v2' # 'LLM360/Amber' # 
-
-    base_model = AutoModelForCausalLM.from_pretrained(base_model_name, torch_dtype=torch.bfloat16)
-    ft_model = AutoModelForCausalLM.from_pretrained(ft_model_name, torch_dtype=torch.bfloat16)
-
-    # print(statistic(base_model, ft_model, 32))
-
-    # base_model = AutoModelForCausalLM.from_pretrained("/juice4/scr4/nlp/model-tracing/olmo_checkpoint/indp/seed_0_100M/")
-    # ft_model = AutoModelForCausalLM.from_pretrained("/juice4/scr4/nlp/model-tracing/olmo_checkpoint/indp/seed_42_100M/")
-    print(statistic(base_model, ft_model, 32))
-
-    # print(cos_matrices(base_model.state_dict()['model.layers.0.mlp.gate_proj.weight'].T, ft_model.state_dict()['model.layers.0.mlp.gate_proj.weight'].T))
-    # print(cos_matrices(base_model.state_dict()['model.embed_tokens.weight'].T, ft_model.state_dict()['model.embed_tokens.weight'].T))
-    # print(cos_matrices(base_model.state_dict()['lm_head.weight'].T, ft_model.state_dict()['lm_head.weight'].T))
--- tracing/statistics/csw_sp.py
+++ tracing/statistics/csw_sp.py
@@ -1,31 +1,25 @@
 import torch
-from transformers import AutoModelForCausalLM
 
-from tracing.utils.utils import cossim, spcor
+from tracing.utils.utils import cossim
 import scipy 
 import numpy as np
 from scipy.stats import chi2
 
 def statistic(base_model, ft_model):
-    return cos_cor_fisher(base_model, ft_model)
+    return csw_sp(base_model, ft_model)
 
-def cos_cor_layer_2d(base_model,ft_model,layer_name):
+def csw_sp_layer(base_model,ft_model,layer_name):
 
     base_mat = base_model.state_dict()[layer_name].T
     ft_mat = ft_model.state_dict()[layer_name].T
 
-    # if 'embed' in layer_name or 'lm_head' in layer_name:
-    #     base_mat = base_mat[:50280]
-    #     ft_mat = ft_mat[:50280]
-
     matched = torch.argmax(cossim(base_mat,ft_mat),axis=-1)
-
     orig = torch.arange(len(matched))
 
     cor, pvalue = scipy.stats.pearsonr(matched.tolist(), orig.tolist())
     return pvalue
 
-def cos_cor_fisher(model1,model2):
+def csw_sp(model1,model2):
 
     chi_squared = 0
     num_layers = 0
@@ -50,28 +44,13 @@ def cos_cor_fisher(model1,model2):
             raise ValueError(
                 f"Model parameter shapes do not match for {name1}: {param1.shape} != {param2.shape}"
             )
-        pvalue = cos_cor_layer_2d(model1, model2, name1)
+        pvalue = csw_sp_layer(model1, model2, name1)
         if not np.isnan(pvalue):
             chi_squared -= 2 * np.log(pvalue)
             num_layers += 1
         p_values.append(pvalue)
 
-        # print(name1, pvalue)
+        print(name1, pvalue)
 
-    p_value = chi2.sf(chi_squared, df=2*num_layers)
-    # print(p_value)
-    # print(p_values)
-    return p_value, p_values
-        
-if __name__ == "__main__":
-
-    base_model_name = 'openlm-research/open_llama_7b' # 'lmsys/vicuna-7b-v1.5' # "meta-llama/Llama-2-7b-hf"
-    ft_model_name = 'openlm-research/open_llama_7b_v2' # 'LLM360/Amber' # "lmsys/vicuna-7b-v1.1" # "codellama/CodeLlama-7b-hf"
-
-    # base_model = AutoModelForCausalLM.from_pretrained(base_model_name, torch_dtype=torch.bfloat16)
-    # ft_model = AutoModelForCausalLM.from_pretrained(ft_model_name, torch_dtype=torch.bfloat16)
-
-    base_model = AutoModelForCausalLM.from_pretrained("/juice4/scr4/nlp/model-tracing/olmo_checkpoint/indp/seed_0_100M/")
-    ft_model = AutoModelForCausalLM.from_pretrained("/juice4/scr4/nlp/model-tracing/olmo_checkpoint/indp/seed_42_100M/")
-
-    statistic(base_model, ft_model)
+    aggregate_pvalue = chi2.sf(chi_squared, df=2*num_layers)
+    return aggregate_pvalue, p_values
