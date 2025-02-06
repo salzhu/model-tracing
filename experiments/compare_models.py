4eed677610a6db40620f2aab58afc97287c43876
salzhu <163059861+salzhu@users.noreply.github.com>
2025-02-05 18:32:42 -0800

    Update and rename compare_models.py to experiments/compare_models.py

--- experiments/compare_models.py
+++ experiments/compare_models.py
@@ -1,3 +1,11 @@
+"""
+Localized testing experiments for two models. 
+Runs \phi_MATCH on all pairs of GLU MLPs between two models and identifies a match. 
+Also can uncomment code to print the aligned activations. 
+
+To run: Use HuggingFace model Ids in Lines 104-05. 
+"""
+
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig
 
@@ -32,32 +40,22 @@ def mlp_matching_gate(base_model, ft_model, dataloader, i,j):
     base_mat.to('cuda')
     ft_mat.to('cuda')
 
-    print(ft_mat.shape)
-    
     base_mat = base_mat.view(-1,base_mat.shape[-1]).T
     ft_mat = ft_mat.view(-1,ft_mat.shape[-1]).T
 
-    print(ft_mat.shape)
+    # If want to print the activations matching for localized testing (See Llama3.2-3B and Llama3.1-8B activation matching experiment)
 
+    """
     ft_mat = torch.norm(ft_mat,dim=1)
-    print(ft_mat.shape)
     sorted = torch.sort(torch.argsort(ft_mat)[:8192])[0]
     for i in sorted:
         print(i.item(),end=" ")
+    """
 
     base_handle.remove()
     ft_handle.remove()
 
     perm = match_wmats(base_mat,ft_mat)
-
-    # mat = cossim(ft_mat,base_mat)
-    # perm = torch.argmax(mat,axis=-1)
-
-    # Alternatively: Using cosine similarity matrix and taking argmax (does not work as well)
-    """
-    mat = cossim(base_mat,ft_mat)
-    perm = torch.argmax(cossim(base_mat,ft_mat),axis=-1)
-    """
     
     return perm
 
@@ -86,12 +84,6 @@ def mlp_matching_up(base_model, ft_model, dataloader, i,j):
     ft_handle.remove()
 
     perm = match_wmats(base_mat,ft_mat)
-
-    # Alternatively: Using cosine similarity matrix and taking argmax (does not work as well)
-    """
-    mat = cossim(base_mat,ft_mat)
-    perm = torch.argmax(cossim(base_mat,ft_mat),axis=-1)
-    """
     
     return perm
 
@@ -101,19 +93,16 @@ def mlp_layers(base_model,ft_model,dataloader,i,j):
     gate_match = mlp_matching_gate(base_model, ft_model, dataloader, i,j)
     up_match = mlp_matching_up(base_model, ft_model, dataloader, i,j)
 
-    print(len(gate_match))
-
     for g in gate_match:
         print(g.item(),end=' ')
 
     cor, pvalue = scipy.stats.pearsonr(gate_match.tolist(), up_match.tolist())
-    # print(i, pvalue, len(gate_match))
 
     return pvalue
 
 def main():
-    model_1_id = "meta-llama/Llama-2-7b-hf" # "meta-llama/Llama-3.2-3B"
-    model_2_id = "princeton-nlp/Sheared-LLaMA-2.7B" # "meta-llama/Llama-3.1-8B"
+    model_1_id = "meta-llama/Llama-2-7b-hf" 
+    model_2_id = "princeton-nlp/Sheared-LLaMA-2.7B" 
 
     print(model_1_id, model_2_id)
 
@@ -122,11 +111,8 @@ def main():
 
     tokenizer = AutoTokenizer.from_pretrained(model_1_id)
 
-    # dataset = prepare_hf_dataset("dlwh/wikitext_103_detokenized",512,tokenizer) 
-    # dataloader = prepare_hf_dataloader(dataset,1)
-
-    dataset = prepare_random_sample_dataset(20, 512)
-    dataloader = prepare_hf_dataloader(dataset, 512)
+    dataset = prepare_hf_dataset("dlwh/wikitext_103_detokenized",512,tokenizer) 
+    dataloader = prepare_hf_dataloader(dataset,1)
 
     print(model_1.config.num_hidden_layers, model_2.config.num_hidden_layers)
 
